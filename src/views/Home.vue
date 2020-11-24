41a9da32a3680221482dd0c6313531aa71c0af63
白濱淳也 <juuuunyaaaa070120@gmail.com>
2020-11-24 10:13:53 +0900

    add docs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-  <v-container id="home">
-    <h1>Home</h1>
+  <v-container >
+    <v-row justify="center">
+    <h1>Haystack Q&Aの使い方</h1>
+    </v-row>
+    <v-row justify="center">
+      <ol>
+        <li>メニューの<v-icon>{{'mdi-text-box'}}</v-icon>Choose dataを選んでデータを選択してください</li>
+        <li>選択後、<v-btn x-small rounded color='primary'>次に進む</v-btn>またはメニューの<v-icon>{{'mdi-help-box'}}</v-icon>Question and Answeringを選択</li>
+        <li>質問文を記入後<v-btn x-small rounded  color='primary'>Send</v-btn></li>
+        <li>答えを確認してください。詳細を知りたい場合<v-btn x-small rounded color='primary'>詳細</v-btn>をクリック</li>
+        <li>他の回答を確認したい場合はカードをクリックしてください</li>
+      </ol>
+    </v-row>
+
   </v-container>
 </template>
 
